eb501d7345ef8da5f898c9ea5c120e6d044be975
Victor Kazankov <victor.kazankov@qatestlab.eu>
2021-03-30 16:14:06 +0300

    Init project
    ##################
    
    Create project structure

---
+++ pages/better_page.py
@@ -0,0 +1,2 @@
+class BetterPage:
+    pass
---
+++ tests/test_hot_page.py
@@ -0,0 +1,14 @@
+def is_dispalayed_hot_page():
+    pass
+
+
+def is_displayed_login_form():
+    pass
+
+
+def is_displayed_comment_day():
+    pass
+
+
+def is_absent_data():
+    pass
