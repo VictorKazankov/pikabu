0bd40b6fcd5ffab7380fab0c5c0ba267b43e4b0a
Victor Kazankov <victor.kazankov@qatestlab.eu>
2021-04-12 12:45:10 +0300

    Add TestFilterPosts class for some tests ################## 1. Choose icon locator 2. Added setup_class method to test_hot_page.py 3. Added TestFoldPosts class to test_hot_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class GeneralLocators():
-    ICON_SITE = (By.XPATH, "//i[@class='sprite sprite--ui__logo']")
+    ICON_SITE = (By.XPATH, "//div[@class='logo hint hint_bottom']")
     LOGIN_FORM = (By.XPATH, "//div[@class='tabs sidebar-auth']/parent::*")
     COMMENTS_DAY_BLOG = (By.XPATH, "//div[@class='sidebar-block__content sidebar-comment-day']/parent::*")
     FILTER_BUTTON = (By.XPATH, "//span[@class='button-filter__inner']")
--- tests/test_hot_page.py
+++ tests/test_hot_page.py
@@ -5,13 +5,14 @@ from pages.hot_page import HotPage
 hot_page_url = "https://pikabu.ru/"
 
 
-class TestGeneralHotPage:
-    @pytest.fixture(scope="class", autouse=True)
-    def setup(self, browser):
-        hot_page = HotPage(browser, hot_page_url)
-        hot_page.open()
-        return hot_page
+@pytest.fixture(scope="function", autouse=True)
+def setup_class(browser):
+    hot_page = HotPage(browser, hot_page_url)
+    hot_page.open()
+    return hot_page
 
+
+class TestGeneralHotPage:
     def test_is_dispalayed_icon_login_comments_elements(self, browser):
         hot_page = HotPage(browser, hot_page_url)
         assert hot_page.is_displayed_pikabu_icon()
@@ -25,12 +26,6 @@ class TestGeneralHotPage:
 
 
 class TestFilterPosts:
-    # @pytest.fixture(scope="class", autouse=True)
-    # def setup(self, browser):
-    #     hot_page = HotPage(browser, hot_page_url)
-    #     hot_page.open()
-    #     return hot_page
-
     def test_opening_calendar(self, browser):
         hot_page = HotPage(browser, hot_page_url)
         hot_page.open_filter_popup()
@@ -57,25 +52,28 @@ class TestFilterPosts:
         hot_page.click_show_posts_button(success_button)
         hot_page.posts_should_display_for_certain_date()
 
-    #
-    # # class TestFoldPosts:
-    # def test_is_displayed_show_in_viewed_posts(hot_page):
-    #     hot_page.open_filter_popup()
-    #     hot_page.is_displayed_show_text()
-    #
-    # def test_is_displayed_view_type(hot_page):
-    #     hot_page.open_filter_popup()
-    #     hot_page.open_view_type_list()
-    #     hot_page.all_types_should_be_displayed()
-    #
-    # @pytest.mark.xfail(reason="Posts aren't folded, after opening them in new tabs")
-    # def test_open_three_posts_in_new_separate_tabs(hot_page):
-    #     url_posts_list = hot_page.get_list_url_for_all_articles()
-    #     # open 3 top posts in new tabs
-    #     hot_page.open_post_to_new_tab(url_posts_list[0])
-    #     hot_page.open_post_to_new_tab(url_posts_list[1])
-    #     hot_page.open_post_to_new_tab(url_posts_list[2])
-    #     hot_page.all_new_tabs_should_be_opened()
-    #     hot_page.posts_should_be_with_preview()
-    #     hot_page.change_fold_type_in_view_panel()
-    #     hot_page.three_posts_should_be_folded()
+
+class TestFoldPosts:
+    def test_is_displayed_show_in_viewed_posts(self, browser):
+        hot_page = HotPage(browser, hot_page_url)
+        hot_page.open_filter_popup()
+        hot_page.is_displayed_show_text()
+
+    def test_is_displayed_view_type(self, browser):
+        hot_page = HotPage(browser, hot_page_url)
+        hot_page.open_filter_popup()
+        hot_page.open_view_type_list()
+        hot_page.all_types_should_be_displayed()
+
+    @pytest.mark.xfail(reason="Posts aren't folded, after opening them in new tabs")
+    def test_open_three_posts_in_new_separate_tabs(self, browser):
+        hot_page = HotPage(browser, hot_page_url)
+        url_posts_list = hot_page.get_list_url_for_all_articles()
+        # open 3 top posts in new tabs
+        hot_page.open_post_to_new_tab(url_posts_list[0])
+        hot_page.open_post_to_new_tab(url_posts_list[1])
+        hot_page.open_post_to_new_tab(url_posts_list[2])
+        hot_page.all_new_tabs_should_be_opened()
+        hot_page.posts_should_be_with_preview()
+        hot_page.change_fold_type_in_view_panel()
+        hot_page.three_posts_should_be_folded()
